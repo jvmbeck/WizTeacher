7f41272474bfe4ee3b8a51d164eff718001f6bfe
jvmbeck <jmadridbeck@gmail.com>
2025-05-16 10:22:24 -0300

    routes for class details and new class pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,24 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', name: 'LoginPage', component: () => import('src/pages/LoginPage.vue') }],
   },
+  {
+    path: '/addNewClass',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', name: 'NewClassPage', component: () => import('src/pages/NewClassPage.vue') },
+    ],
+  },
+  {
+    path: '/class/:id',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'ClassDetails',
+        component: () => import('src/pages/ClassDetailsPage.vue'),
+      },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
