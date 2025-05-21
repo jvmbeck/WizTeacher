04009402fd37e93f0635503fd0bebaab7abdfb64
jvmbeck <jmadridbeck@gmail.com>
2025-05-21 10:00:58 -0300

    moved method to authServices

--- src/services/TeacherServices.js
+++ src/services/TeacherServices.js
@@ -1,24 +1,7 @@
-import { doc, getDoc, collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../key/configKey.js' // Adjust the path as necessary
 
 const TeacherServices = {
-  async getSignedInUserInfo(userId) {
-    try {
-      const docRef = doc(db, 'teachers', userId) // Adjust the collection name as necessary
-      const docSnap = await getDoc(docRef)
-
-      if (docSnap.exists()) {
-        return docSnap // Return the document snapshot
-      } else {
-        console.log('No such document!')
-        return null
-      }
-    } catch (error) {
-      console.error('Error fetching user info:', error)
-      throw error
-    }
-  },
-
   async fetchAllTeachers() {
     const snapshot = await getDocs(collection(db, 'teachers'))
     const teachers = []
