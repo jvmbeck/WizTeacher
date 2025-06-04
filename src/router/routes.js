fb46dc3d0b2236900b4c2bcfd26b80799868e3a7
jvmbeck <jmadridbeck@gmail.com>
2025-06-04 15:41:52 -0300

    fixed paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -13,22 +13,17 @@ const routes = [
       {
         path: '',
         name: 'TeacherDashboard',
-        component: () => import('src/pages/TeacherDashboard.vue'),
-      },
-      {
-        path: 'addNewClass',
-        name: 'NewClassPage',
-        component: () => import('src/pages/NewClassPage.vue'),
+        component: () => import('src/pages/teacher/TeacherDashboard.vue'),
       },
       {
         path: 'class/:id',
         name: 'ClassDetails',
-        component: () => import('src/pages/ClassDetailsTeacher.vue'),
+        component: () => import('src/pages/teacher/ClassDetails.vue'),
       },
       {
         path: 'classList',
         name: 'ClassListTeacher',
-        component: () => import('src/pages/ClassListPage.vue'),
+        component: () => import('src/pages/admin/classes/ClassListPage.vue'),
       },
     ],
   },
@@ -38,26 +33,30 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     meta: { requiresAuth: true, role: 'admin' },
     children: [
-      { path: '', name: 'AdminDashboard', component: () => import('src/pages/AdminDashboard.vue') },
+      {
+        path: '',
+        name: 'AdminDashboard',
+        component: () => import('src/pages/admin/AdminDashboard.vue'),
+      },
       {
         path: 'studentList',
         name: 'StudentList',
-        component: () => import('src/pages/StudentListPage.vue'),
+        component: () => import('src/pages/admin/students/StudentListPage.vue'),
       },
       {
         path: 'studentDetails/:studentId',
         name: 'studentDetails',
-        component: () => import('src/pages/StudentDetailsPage.vue'),
+        component: () => import('src/pages/admin/students/StudentDetailsPage.vue'),
       },
       {
         path: 'classList',
         name: 'classList',
-        component: () => import('src/pages/ClassListPage.vue'),
+        component: () => import('src/pages/admin/classes/ClassListPage.vue'),
       },
       {
         path: 'classDetails/:classId',
         name: 'classDetailsAdmin',
-        component: () => import('src/pages/ClassDetailsAdmin.vue'),
+        component: () => import('src/pages/admin/classes/ClassDetailsAdmin.vue'),
       },
     ],
   },
