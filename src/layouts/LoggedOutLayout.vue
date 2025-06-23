3882e883f6167da2d35fdaa968e8be3b884e3bb7
jvmbeck <jmadridbeck@gmail.com>
2025-06-23 14:11:39 -0300

    added new layouts for logged in users

--- src/layouts/LoggedOutLayout.vue
+++ src/layouts/LoggedOutLayout.vue
@@ -3,8 +3,6 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title> WizTeacher </q-toolbar-title>
-
-        <div><q-avatar color="primary" text-color="white">J</q-avatar></div>
       </q-toolbar>
     </q-header>
 
